a33a5aa689507001e52ef7a77d62bd0e3bac7073
EmilyDSarani <emily.sarani@gmail.com>
2022-01-28 15:23:17 -0600

    egypt get all done

--- __tests__/egyptians.test.js
+++ __tests__/egyptians.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const Egyptian = require('../lib/models/Egyptian');
+const Egyptian = require('../lib/models/Egyptian');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -12,12 +12,24 @@ describe('backend routes', () => {
   afterAll(() => {
     pool.end();
   });
+
   it('should create an egyptian god/goddess', async() => {
     const res = await request(app)
       .post('/api/v1/egyptians')
-      .send({ name: 'Bastet', title: 'Moon and Hunt', animal: 'Cat' });
+      .send({ name: 'Bastet', title: 'Fierce Warrior', animal: 'Cat' });
 
-    expect(res.body).toEqual({ id: expect.any(String), name: 'Bastet', title: 'Moon and Hunt', animal: 'Cat' });
+    expect(res.body).toEqual({ id: expect.any(String), name: 'Bastet', title: 'Fierce Warrior', animal: 'Cat' });
   });
 
+  it('should get all gods/goddess', async() => {
+    await Egyptian.insert({
+      name: 'Bastet', title: 'Fierce Warrior', animal: 'Cat'
+    });
+
+    const res = await request(app).get('/api/v1/egyptians');
+    expect(res.body).toEqual([{ id: expect.any(String),
+      name: 'Horus', title: 'the Avenger', animal: 'Falcon'
+    },
+    { id: expect.any(String), name: 'Bastet', title: 'Fierce Warrior', animal: 'Cat' }]);
+  });
 });
