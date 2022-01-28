34a68936ec975bd41a4eb0bac1a4ff4ef13c4f9d
EmilyDSarani <emily.sarani@gmail.com>
2022-01-28 12:44:43 -0600

    get all completed

--- __tests__/greeks.test.js
+++ __tests__/greeks.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Greek = require('../lib/models/Greek');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -19,6 +20,19 @@ describe('backend routes', () => {
 
     expect(res.body).toEqual({ id: expect.any(String), name: 'Ares', title: 'God of War', romanName: 'Mars'
     });
+  });
+
+  it('should get all gods/goddess', async() => {
+    await Greek.insert({
+      name: 'Ares', title: 'God of War', romanName: 'Mars' 
+    });
+    const res = await request(app).get('/api/v1/greeks');
+    expect(res.body).toEqual([{ id: expect.any(String),
+      name: 'Zeus',
+      romanName: 'Jupiter',
+      title: 'God of Thunder',
+    },
+    { id: expect.any(String), name: 'Ares', title: 'God of War', romanName: 'Mars' }]);
   });
 
 });
