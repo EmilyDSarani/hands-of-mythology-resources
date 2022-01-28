b546b81c602fb6d94dc8dc22570f319760ec886b
EmilyDSarani <emily.sarani@gmail.com>
2022-01-28 14:07:48 -0600

    norse get all done

--- __tests__/norses.test.js
+++ __tests__/norses.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const Norse = require('../lib/models/Norse');
+const Norse = require('../lib/models/Norse');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -21,4 +21,18 @@ describe('backend routes', () => {
     expect(res.body).toEqual({ id: expect.any(String), name: 'Frigg', characteristic: 'Beautiful and Protective', power: 'Goddess of Love and Beauty'
     });
   });
+  it('should get all gods/goddess', async() => {
+    await Norse.insert({
+      name: 'Frigg', characteristic: 'Beautiful and Protective', power: 'Goddess of Love and Beauty'
+    });
+
+    const res = await request(app).get('/api/v1/norses');
+    expect(res.body).toEqual([{ id: expect.any(String),
+      name: 'Odin', characteristic: 'One-Eye', power: 'God of War, Poetry, and Magic'
+    },
+    { id: expect.any(String), name: 'Frigg', characteristic: 'Beautiful and Protective', power: 'Goddess of Love and Beauty' }]);
+  });
+
+
+
 });
