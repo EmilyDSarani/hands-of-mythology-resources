91a364d8239a88e502810f7ef73b80c4c61e613a
EmilyDSarani <emily.sarani@gmail.com>
2022-01-28 18:20:57 -0600

    get all persians passing

--- __tests__/persians.test.js
+++ __tests__/persians.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const Persian = require('../lib/models/Persian');
+const Persian = require('../lib/models/Persian');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -19,6 +19,16 @@ describe('backend routes', () => {
       .send({ name: 'Anahita ', title: 'Goddess of Water', funFact: 'Rides a Chariot of 4 horses: wind, rain, cloud, and sleet' });
 
     expect(res.body).toEqual({ id: expect.any(String), name: 'Anahita ', title: 'Goddess of Water', funFact: 'Rides a Chariot of 4 horses: wind, rain, cloud, and sleet' });
+  });
+  it('should get all gods/goddess', async() => {
+    await Persian.insert({
+      name: 'Anahita ', title: 'Goddess of Water', funFact: 'Rides a Chariot of 4 horses: wind, rain, cloud, and sleet'
+    });
+
+    const res = await request(app).get('/api/v1/persians');
+    expect(res.body).toEqual([{ id: expect.any(String), name: 'Atar', title: 'God of Fire and Element itself', funFact: 'Depicted as flames'
+    },
+    { id: expect.any(String), name: 'Anahita ', title: 'Goddess of Water', funFact: 'Rides a Chariot of 4 horses: wind, rain, cloud, and sleet' }]);
   });
 
 
